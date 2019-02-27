66402402861a34572ea97aab98e17913bc06f0ec
TB831 <druu831@gmail.com>
2019-02-27 09:36:58 -0800

    added SignedInLinks component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
 
 const NavBar = () => {
   return (
-    <h1>Nav!</h1>
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to="/" className="brand-logo">Mario Plan</Link>
+        <SignedInLinks />
+      </div>
+    </nav>
   );
 }
  
