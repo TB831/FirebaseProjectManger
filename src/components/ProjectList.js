4275723a0cf581d10ffda94c79577d5e00a644c6
TB831 <druu831@gmail.com>
2019-02-27 10:29:02 -0800

    Added ProjectSummary component. Nested 3 into ProjectList

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,29 +1,12 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grew-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Andrew Sanchez</p>
-          <p className="grey-text">27th February, 10:16AM</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grew-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Andrew Sanchez</p>
-          <p className="grey-text">27th February, 10:16AM</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grew-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Andrew Sanchez</p>
-          <p className="grey-text">27th February, 10:16AM</p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 }
