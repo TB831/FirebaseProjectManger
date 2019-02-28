35348ab457e01daa88c24d5bfd067fa8e28e6e88
TB831 <druu831@gmail.com>
2019-02-27 16:34:12 -0800

    destructuring id for ProjectDetails component

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+  const { id } = props.match.params;
+  
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>Elit anim cillum cillum laborum culpa do ea nostrud quis laboris nisi nostrud voluptate. Est non tempor exercitation excepteur nisi aute sunt. Lorem magna dolore voluptate enim sint Lorem eu culpa minim commodo pariatur quis excepteur. Et sint incididunt culpa tempor velit sunt in. Culpa aliquip fugiat tempor sint. Aliquip qui pariatur cillum sint aliquip consectetur occaecat ullamco nulla occaecat anim reprehenderit ad.</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
