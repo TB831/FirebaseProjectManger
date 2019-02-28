a079c9a4afc528e3c19968d5ab9769ece38b0528
TB831 <druu831@gmail.com>
2019-02-27 17:20:08 -0800

    Added SignUp component boilerplate

---
+++ src/containers/SignUp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() { 
+    return (
+      
+    );
+  }
+}
+ 
+export default SignUp;
