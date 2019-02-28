0639206b577c981c4e3e15988ddd4e5fce2ef3e4
TB831 <druu831@gmail.com>
2019-02-28 09:15:50 -0800

    Added Provider from react-redux. Application now has access to our store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer'; // rootReducers which combines reducers
+
 
 
 const store = createStore(rootReducer); // Our single source of truth
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
