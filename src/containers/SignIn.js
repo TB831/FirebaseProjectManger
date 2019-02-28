6848e49b8c362d5760f6e2e64916a0d8543ad625
TB831 <druu831@gmail.com>
2019-02-27 17:18:21 -0800

    Added SignIn container logic

--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -1,13 +1,37 @@
+import React, { Component } from 'react';
+
 class SignIn extends Component {
   constructor() {
     super();
     this.state = {
-
+      email: '',
+      password: ''
     }
   }
+
+  onSubmitHandler = (e) => {
+    e.preventDefault();
+    // console.log(this.state.email, this.state.password);
+  }
+
   render() { 
     return (
-      
+      <div className="container">
+        <form onSubmit={this.onSubmitHandler} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input onChange={(e) => this.setState({ [e.target.id]: e.target.value })} type="email" id="email" />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input onChange={(e) => this.setState({ [e.target.id]: e.target.value })} type="password" id="password" />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Login</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
