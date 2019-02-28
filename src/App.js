add98237fc115564190ba7aaf931af8f9cd38659
TB831 <druu831@gmail.com>
2019-02-27 16:30:43 -0800

    added ProjectDetails component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Dashboard from './containers/Dashboard';
+import ProjectDetails from './components/ProjectDetails';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div>
           <NavBar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
