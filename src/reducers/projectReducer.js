99e2b4de53d116602a63659cd5cefe7ca3442566
TB831 <druu831@gmail.com>
2019-02-28 17:06:51 -0800

    setup projectReducer logic for our projectActions

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -8,9 +8,15 @@ const initState = { // Dummy data
 
 const projectReducer = (state=initState, action) => {
   switch (action.type) {
-    case 'CREATE_PROJECT': console.log('Created Project', action.project)
+    case 'CREATE_PROJECT': 
+      console.log('Created Project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 export default projectReducer;
