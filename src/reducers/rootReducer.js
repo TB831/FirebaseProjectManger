0d352358817073cb748b9565992d143c7f5a74a7
TB831 <druu831@gmail.com>
2019-03-01 12:31:54 -0800

    added firebaseReducer to our rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer // Responsible for syncing our data on firestore
+  firestore: firestoreReducer, // Responsible for syncing our data on firestore
+  firebase: firebaseReducer // Sync our auth status on firebase with our app
 })
 
 export default rootReducer;
