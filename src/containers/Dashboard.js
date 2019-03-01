f3c032fe47a949d256dbfa789b59f42b16c798ed
TB831 <druu831@gmail.com>
2019-03-01 09:58:04 -0800

    added firestore reducer to our root reducer. Added firestoreConnect to sync with our firestoreReducer. mapStateToProps syncs with our firestore

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Notifications from '../components/Notifications';
 import ProjectList from '../components/ProjectList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 class Dashboard extends Component {
   render() {
@@ -24,8 +26,13 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    projects: state.project.projects
+    projects: state.firestore.ordered.projects
   }
 }
  
-export default connect(mapStateToProps)(Dashboard);
+export default compose( // Use compose for multiple higher order components
+  connect(mapStateToProps),
+  firestoreConnect([    // Takes in array as parameter
+    { collection: 'projects'}
+  ])
+)(Dashboard);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  project: projectReducer
+  project: projectReducer,
+  firestore: firestoreReducer // Responsible for syncing our data on firestore
 })
 
 export default rootReducer;
