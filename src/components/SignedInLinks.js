bdce5e8ffa94daba352c789028a1bcb06267c52a
TB831 <druu831@gmail.com>
2019-03-01 17:54:35 -0800

    subscribed NavBar to profile data from firebase. Passed down profile data to SignInLinks from NavBar

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import { signOut } from '../actions/authActions';
 
 const SignedInLinks = (props) => {
-  const { signOut } = props;
+  const { signOut, profile } = props;
 
   return (
     <ul className="right">
       <li><NavLink to="/create">New Project</NavLink></li>
       <li><a onClick={signOut}>Log out</a></li>
-      <li><NavLink to="/" className="btn btn-floating green lighten-1">AS</NavLink></li>
+      <li><NavLink to="/" className="btn btn-floating green lighten-1">{profile.initials}</NavLink></li>
     </ul>
   )
 }
