e4661206421e0473e1f30e7fe139948be07d9f7b
TB831 <druu831@gmail.com>
2019-03-01 18:13:19 -0800

    CreateProject component reroutes after submission. bug change on ProjectSummary component

--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const ProjectSummary = (props) => {
-  const { title, authorFirstName, authorLastName, } = props.project;
+  const { title, authorFirstname, authorLastName, } = props.project;
 
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grew-text text-darken-3">
         <span className="card-title">{title}</span>
-        <p>Posted by {authorFirstName} {authorLastName}</p>
+        <p>Posted by {authorFirstname} {authorLastName}</p>
         <p className="grey-text">27th February, 10:16AM</p>
       </div>
     </div>
--- src/containers/CreateProject.js
+++ src/containers/CreateProject.js
@@ -15,6 +15,7 @@ class CreateProject extends Component {
   onSubmitHandler = (e) => {
     e.preventDefault();
     this.props.createProject(this.state);
+    this.props.history.push('/');
   }
 
   render() {
