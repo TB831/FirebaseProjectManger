fc7e69eb24d8e121b60c7c6a40fa58e90331861b
TB831 <druu831@gmail.com>
2019-03-01 17:01:57 -0800

    Added route guards to SignIn and SignUp for when a user is signed in. Connected SignUp to redux and added mapStateToProps

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 class SignUp extends Component {
   constructor() {
@@ -16,7 +18,13 @@ class SignUp extends Component {
     // console.log(this.state);
   }
 
-  render() { 
+  render() {
+    const { auth } = this.props;
+
+    if (auth.uid) {
+      return <Redirect to="/" /> 
+    }
+
     return (
       <div className="container">
         <form onSubmit={this.onSubmitHandler} className="white">
@@ -45,5 +53,11 @@ class SignUp extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth
+  }
+}
  
-export default SignUp;
+export default connect(mapStateToProps)(SignUp);
