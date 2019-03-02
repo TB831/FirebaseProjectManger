090ee1247c8d20fd2c864b44821c9b74fbb98e9a
TB831 <druu831@gmail.com>
2019-03-01 18:06:38 -0800

    ProjectSummary component outputs dynamic data

--- src/components/ProjectSummary.js
+++ src/components/ProjectSummary.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const ProjectSummary = (props) => {
-  const { project } = props;
+  const { title, authorFirstName, authorLastName, } = props.project;
 
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grew-text text-darken-3">
-        <span className="card-title">{project.title}</span>
-        <p>Posted by Andrew Sanchez</p>
+        <span className="card-title">{title}</span>
+        <p>Posted by {authorFirstName} {authorLastName}</p>
         <p className="grey-text">27th February, 10:16AM</p>
       </div>
     </div>
