cd76444b185f1475ae54de720368f36f33231048
TB831 <druu831@gmail.com>
2019-03-01 17:14:29 -0800

    action created for signUp in authActions

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -23,3 +23,27 @@ export const signOut = () => {
     })
   }
 }
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, { getFirebase, getFireStore }) => {
+    const firebase = getFirebase();
+    const firestore = getFireStore();
+    firebase.auth().createUserWithEmailAndPassword(
+      newUser.email,
+      newUser.password
+    )
+    .then((res) => {  // Responsible for creating a new user in firestore
+      return firestore.collection('users').doc(res.user.uid).set({    //.doc(res.user.uid) get us the user id from firebase, that way id is the same for firestore and firebase.
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0] + newUser.lastName[0]
+      })
+    })
+    .then(() => { // Responsible for dispatching our action
+      dispatch({ type: 'SIGNUP_SUCCESS' })
+    })
+    .catch((err) => { // Responsible for catching any errors when making a new user
+      dispatch({ type: 'SIGNUP_ERROR', err })
+    })
+  }
+}
